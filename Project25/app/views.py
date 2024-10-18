60c8332eaded385fd328250e1183bd6d40c57e86
adityarath14 <adityarath14042001@gmail.com>
2024-10-19 00:34:49 +0530

    DjangoHTML_Forms

---
+++ Project25/app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from app.models import *
+def insertTopic(request):
+    if request.method=='POST':
+        tn=request.POST['tn']
+        TO=Topic.objects.get_or_create(topic_name=tn)
+        return HttpResponse(f'{tn}Topic is Created')
+    return render(request,'insertTopic.html')
+def insertWebPage(request):
+    if request.method=='POST':
+        tn=request.POST['tn']
+        nm=request.POST['nm']
+        ul=request.POST['ul']
+        el=request.POST['el']
+        TO=Topic.objects.get(topic_name=tn)
+        WO=WebPage.objects.get_or_create(topic_name=TO,name=nm,url=ul,email=el)
+        return HttpResponse('Webpage is Created')
+    return render(request,'insertWebPage.html')
+def insertAccessRecord(request):
+    if request.method=='POST':
+        nm=request.POST['nm']
+        ar=request.POST['ar']
+        dt=request.POST['dt']
+        WO=WebPage.objects.get(name=nm)
+        AO=AccessRecord.objects.get_or_create(name=WO,author=ar,date=dt)
+        return HttpResponse('AccessRecord is Created')
+    return render(request,'insertAccessRecord.html')
