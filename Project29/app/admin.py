9b6af33bd32118a5586cc2146194beaca31327b5
adityarath14 <adityarath14042001@gmail.com>
2024-10-27 16:52:15 +0530

    DjangoForm

--- Project29/app/admin.py
+++ Project29/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app.models import*
+from app.models import *
 # Register your models here.
 admin.site.register(Topic)
 admin.site.register(WebPage)
--- Project29/app/forms.py
+++ Project29/app/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from app.models import *
 class TopicForm(forms.Form):
-    tn=forms.CharField()
+    tn=forms.CharField(label='Topic Name')
 class WebPageForm(forms.Form):
-    tn=forms.ModelChoiceField(queryset=Topic.objects.all())
-    nm=forms.CharField()
-    ul=forms.URLField()
-    el=forms.EmailField()
+    tn=forms.ModelChoiceField(queryset=Topic.objects.all(),label='Topic Name')
+    nm=forms.CharField(label='Name')
+    ul=forms.URLField(label='URL')
+    el=forms.EmailField(label='Email')
 class AccessRecordForm(forms.Form):
-    nm=forms.ModelChoiceField(queryset=WebPage.objects.all())
-    au=forms.CharField()
-    dt=forms.DateField()
+    nm=forms.ModelChoiceField(queryset=WebPage.objects.all(),label='Name')
+    au=forms.CharField(label='Author')
+    dt=forms.DateField(label='Date')
