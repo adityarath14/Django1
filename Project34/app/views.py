b77a15566237ccf557a2a412c06d210e5f068e62
adityarath14 <adityarath14042001@gmail.com>
2024-11-13 19:37:27 +0530

    User_Registreation_Process

--- Project34/app/views.py
+++ Project34/app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from app.forms import *
+from django.http import HttpResponse
 # Create your views here.
 def registration(request):
     EUFO=UserForm()
     EPFO=ProfileForm()
     d={'EUFO':EUFO, 'EPFO':EPFO}
+    if request.method=='POST' and request.FILES:
+        NMUFDO=UserForm(request.POST)
+        NMPFDO=ProfileForm(request.POST, request.FILES)
+        if NMUFDO.is_valid() and NMPFDO.is_valid():
+            MUFDO=NMUFDO.save(commit=False)
+            pw=NMUFDO.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+            MPFDO=NMPFDO.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            return HttpResponse('Registeration is Successfully')
+        else:
+            return HttpResponse('Invalid Data')
     return render(request,'registration.html',d)
