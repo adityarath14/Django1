9d5d7eb6f96c46d46cbacdd4be72a89849311a4e
adityarath14 <adityarath14042001@gmail.com>
2024-11-16 12:25:31 +0530

    User_Registration_Process

--- Project34/Project34/urls.py
+++ Project34/Project34/urls.py
@@ -22,4 +22,7 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
+    path('home/',home,name='home'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- Project34/app/views.py
+++ Project34/app/views.py
@@ -1,8 +1,9 @@
-
 from django.shortcuts import render
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def registration(request):
     EUFO=UserForm()
@@ -24,3 +25,27 @@ def registration(request):
         else:
             return HttpResponse('Invalid Data')
     return render(request,'registration.html',d)
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        AUO=authenticate(username=username,password=password)
+        if AUO:
+            if AUO.is_active:
+                login(request,AUO)
+                request.session['username']=username
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse('Not an Active User')
+        return HttpResponse('Invalid Credentials')
+    return render(request,'user_login.html')
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
