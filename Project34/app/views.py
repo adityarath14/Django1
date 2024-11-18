1eea87c0b9a3c546271fc9b022c09b8c81bbd336
adityarath14 <adityarath14042001@gmail.com>
2024-11-19 00:17:31 +0530

    User Registration Process

--- Project34/app/views.py
+++ Project34/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from django.core.mail import send_mail
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
@@ -49,3 +50,33 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
+@login_required
+def profile_display(request):
+    username=request.session.get('username')
+    UO=User.objects.get(username=username)
+    PO=Profile.objects.get(username=UO)
+    d={'UO':UO, 'PO':PO}
+    return render(request,'profile_display.html',d)
+@login_required
+def change_password(request):
+    if request.method=='POST':
+        cpw=request.POST['cpw']
+        username=request.session['username']
+        UO=User.objects.get(username=username)
+        UO.set_password(cpw)
+        UO.save()
+        return HttpResponse('Password is Changed')
+    return render(request,'change_password.html')
+def reset_password(request):
+    if request.method=='POST':
+        pw=request.POST['pw']
+        username=request.POST['un']
+        LUO=User.objects.filter(username=username)
+        if LUO:
+            UO=LUO[0]
+            UO.set_password(pw)
+            UO.save()
+            return HttpResponse('Password is Changed')
+        else:
+            return HttpResponse('User is not present')
+    return render(request,'reset_password.html')     
